a76e294e6965abf0c6c77add2c81397be32f3a18
Fadahunsi paul <101594719+Fadahunsi-Paul@users.noreply.github.com>
2023-09-15 23:35:47 +0000

    Update urls.py

--- work/urls.py
+++ work/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('api/',PersonAPIview.as_view()),
-    path('details/<int:id>/',PersonDetails.as_view())
+    path('api/details/<int:id>/',PersonDetails.as_view())
 ]
